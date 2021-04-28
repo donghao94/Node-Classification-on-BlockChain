90fef22c21813607ffd1754875ee466c269855d4
donghao94 <63643301+donghao94@users.noreply.github.com>
2021-04-28 10:50:15 +0200

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,8 +1,8 @@
 import wget
 
 
-url1 = 'https://www.dropbox.com/s/5sg20n4w24df1ni/eth_graph.txt?dl=1'
-url2 = 'https://www.dropbox.com/s/461t6yww3o1rs8x/address_label.txt?dl=1'
+url1 = 'https://www.dropbox.com/s/461t6yww3o1rs8x/address_label.txt?dl=1'
+url2 = 'https://www.dropbox.com/s/5sg20n4w24df1ni/eth_graph.txt?dl=1'
 
 out_fname = './dataset'
 wget.download(url1, out=out_fname)
